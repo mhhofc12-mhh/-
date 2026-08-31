9176815b45ec72757b2ca0d8444e279e6588be3a
mhhofc12-mhh <mhhofc12@gmail.com>
2026-08-31 20:30:08 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,26 +8,22 @@ android {
     namespace = "com.hrm.quranshiktehobei"
     compileSdk = 35
 
-    defaultConfig {
-        applicationId = "com.hrm.quranshiktehobei"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
+defaultConfig {
+    applicationId = "com.hrm.quranshiktehobei"
+    minSdk = 29
+    targetSdk = 35
+    versionCode = 1
+    versionName = "1.0"
+}
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildFeatures {
-        compose = true
-    }
+kotlinOptions {
+    jvmTarget = "17"
+}
 
     packaging {
         resources {
